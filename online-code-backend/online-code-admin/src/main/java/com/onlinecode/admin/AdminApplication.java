52b34f960532efc92074dbc3baa2d8663d19682a
aaron <peng.sun@aiseeding.com>
2024-04-26 19:35:50 +0800

    添加SQL监控

--- online-code-backend/online-code-admin/src/main/java/com/onlinecode/admin/AdminApplication.java
+++ online-code-backend/online-code-admin/src/main/java/com/onlinecode/admin/AdminApplication.java
@@ -1,13 +1,16 @@
 package com.onlinecode.admin;
 
+import com.slowsql.spring.boot.autoconfigure.EnableSlowSqlMonitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
 
 /**
  * @author xuan
  * @since 2020/8/16
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {ElasticsearchRestClientAutoConfiguration.class})
+@EnableSlowSqlMonitor
 public class AdminApplication {
 
     public static void main(String[] args) {
