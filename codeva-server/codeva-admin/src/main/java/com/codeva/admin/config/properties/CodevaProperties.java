4e624ff4b619d95b8649bae9fe6e2935b893648d
zzusp <peng_zzu@163.com>
2024-12-03 22:55:15 +0800

    支持设置weixin的appid和密钥

--- codeva-server/codeva-admin/src/main/java/com/codeva/admin/config/properties/CodevaProperties.java
+++ codeva-server/codeva-admin/src/main/java/com/codeva/admin/config/properties/CodevaProperties.java
@@ -10,9 +10,11 @@ public class CodevaProperties {
     public static final String CODEVA_PREFIX = "codeva";
 
     private Cron cron;
+    private Weixin weixin;
 
     public CodevaProperties() {
         this.cron = new Cron();
+        this.weixin = new Weixin();
     }
 
     public static class Cron {
@@ -27,11 +29,40 @@ public class CodevaProperties {
         }
     }
 
+    public static class Weixin {
+        private String appId;
+        private String appSecret;
+
+        public String getAppId() {
+            return appId;
+        }
+
+        public void setAppId(String appId) {
+            this.appId = appId;
+        }
+
+        public String getAppSecret() {
+            return appSecret;
+        }
+
+        public void setAppSecret(String appSecret) {
+            this.appSecret = appSecret;
+        }
+    }
+
     public Cron getCron() {
         return cron;
     }
 
     public void setCron(Cron cron) {
         this.cron = cron;
+    }
+
+    public Weixin getWeixin() {
+        return weixin;
+    }
+
+    public void setWeixin(Weixin weixin) {
+        this.weixin = weixin;
     }
 }
